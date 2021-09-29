ecd39a84a3ada7fdda2b9b349c4310dd75ce45c6
DenProgMan <rkden58@gmail.com>
2021-09-29 21:36:31 +0300

    feat(basic-js): solve the dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,18 +2,33 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+    if (!Array.isArray(members)) {
+        return false;
+    }
+
+    const resultArray = members.reduce((arr, value) => {
+        if (typeof value === 'string') {
+            arr.push(value.trim()[0].toUpperCase());
+        }
+        return arr;
+    }, []);
+
+    if (!resultArray.length) {
+        return false;
+    }
+
+    resultArray.sort();
+    return resultArray.join('');
 }
