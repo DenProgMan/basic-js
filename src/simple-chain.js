9adac8bca6a54c4eb74d6ae0a31ac6bdfb754bc0
DenProgMan <rkden58@gmail.com>
2021-09-30 10:13:20 +0300

    feat(basic-js): solve the simple chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,27 +2,37 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 export default {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    chain: [],
+    getLength() {
+        return this.chain.length;
+    },
+    addLink(value) {
+        this.chain.push(`( ${value} )`);
+        return this;
+    },
+    removeLink(position) {
+        try {
+            if (this.chain[position - 1]) {
+                this.chain.splice(position - 1, 1);
+                return this;
+            } else {
+                throw new Error('');
+            }
+        } catch (error) {
+            this.chain = [];
+            throw new Error("You can't remove incorrect link!");
+        }
+    },
+    reverseChain() {
+        this.chain.reverse();
+        return this;
+    },
+    finishChain() {
+        const result = this.chain.join('~~');
+        this.chain = [];
+        return result;
+    }
 };
