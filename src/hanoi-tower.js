95b8e1301f643399e5b752a3e0a8b96620c02153
DenProgMan <rkden58@gmail.com>
2021-09-30 08:01:16 +0300

    feat(basic-js): solve the hanoi tower task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -3,18 +3,25 @@ import { NotImplementedError } from '../extensions/index.js';
 /**
  * Calculate turns number and time (in seconds) required
  * to solve puzzle
- * 
+ *
  * @param {Number} disks number of disks
  * @param {Number} turnsSpeed speed (in turns/hour)
  * @return {Object} object with props turns (number of turns)
  * and seconds (time in seconds)
  *
  * @example
- * 
+ *
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-export default function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+const SECONDS_PER_HOUR = 60 * 60;
+
+export default function calculateHanoi(disksNumber, turnsSpeed) {
+    const turns = 2 ** disksNumber - 1;
+    const seconds = Math.floor((turns * SECONDS_PER_HOUR) / turnsSpeed);
+
+    return {
+        turns,
+        seconds
+    };
 }
