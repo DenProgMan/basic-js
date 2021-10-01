57b1ea907e2dea1c6e9fe4bf77edea16ef100503
DenProgMan <rkden58@gmail.com>
2021-10-01 20:22:11 +0300

    feat(basic-js): solve the sort by height task

--- src/st-sort-by-height.js
+++ src/st-sort-by-height.js
@@ -11,7 +11,12 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-export default function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function sortByHeight(arr) {
+    const sorted = [...arr].sort((a, b) => a-b).filter(value => value !== -1);
+
+    return arr.map(value => {
+        return value === -1
+            ? value
+            : sorted.shift();
+    });
 }
