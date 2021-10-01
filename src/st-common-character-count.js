e5fd67b1c52659758d342b4f422cf27f3fffa1f1
DenProgMan <rkden58@gmail.com>
2021-10-01 16:43:20 +0300

    feat(basic-js): solve the common character count task

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -11,7 +11,16 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getCommonCharacterCount(stringFirst, stringSecond) {
+    const s1 = stringFirst.toLowerCase();
+    const s2 = stringSecond.toLowerCase();
+    const unique = new Set(`${s1}${s2}`.split(''));
+    let count = 0;
+
+    unique.forEach(symbol => {
+        // count += Math.min((s1.match(new RegExp(symbol, 'g')) || []).length, (s2.match(new RegExp(symbol, 'g')) || []).length);
+        count += Math.min(s1.split(symbol).length - 1, s2.split(symbol).length - 1);
+    });
+
+    return count;
 }
